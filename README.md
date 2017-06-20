3a58f7a503a71c3a2f84d0cbd66fe57c18cbb9fc
Peter Gribanov <wcode404@gmail.com>
2017-06-20 14:49:01 +0300

    add config files

---
+++ README.md
@@ -0,0 +1,18 @@
+# Generate UID like YouTube
+
+## Installation
+
+Pretty simple with [Composer](http://packagist.org), run:
+
+```sh
+composer require gpslab/base64uid
+```
+
+## Usage
+
+```php
+```
+
+## License
+
+This bundle is under the [MIT license](http://opensource.org/licenses/MIT). See the complete license in the file: LICENSE
---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+$file = __DIR__.'/../vendor/autoload.php';
+
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
+}
+
+require_once $file;
