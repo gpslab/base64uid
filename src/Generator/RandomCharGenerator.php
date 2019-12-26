d48ef79fa7cf06b71e653842fdd156d46dc80073
Peter Gribanov <wcode404@gmail.com>
2019-12-26 12:34:19 +0300

    create RandomCharGenerator

---
+++ src/Generator/RandomCharGenerator.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Base64UID\Generator;
+
+use GpsLab\Component\Base64UID\Exception\InvalidArgumentException;
+
+class RandomCharGenerator
+{
+    /**
+     * @var int
+     */
+    private $uid_length;
+
+    /**
+     * @var string
+     */
+    private $charset;
+
+    /**
+     * @var int
+     */
+    private $charset_size;
+
+    /**
+     * @param int    $uid_length
+     * @param string $charset
+     */
+    public function __construct(
+        $uid_length = 10,
+        $charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-'
+    ) {
+        if (!is_int($uid_length)) {
+            throw new InvalidArgumentException(sprintf('Length of UID should be integer, got "%s" instead.', gettype($uid_length)));
+        }
+        if (!is_string($charset)) {
+            throw new InvalidArgumentException(sprintf('Charset of UID should be a string, got "%s" instead.', gettype($charset)));
+        }
+        if ($uid_length <= 0) {
+            throw new InvalidArgumentException(sprintf('Length of UID should be grate then 0, got "%d" instead.', $uid_length));
+        }
+        if ($charset === '') {
+            throw new InvalidArgumentException('Charset of UID should not be empty.');
+        }
+
+        $this->uid_length = $uid_length;
+        $this->charset = $charset;
+        $this->charset_size = strlen($charset);
+    }
+
+    /**
+     * @return string
+     */
+    public function generate()
+    {
+        $uid = '';
+        for ($i = 0; $i < $this->uid_length; ++$i) {
+            $uid .= $this->charset[random_int(0, $this->charset_size - 1)];
+        }
+
+        return $uid;
+    }
+}
---
+++ tests/Generator/RandomCharGeneratorTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Base64UID\Tests\Generator;
+
+use GpsLab\Component\Base64UID\Generator\RandomCharGenerator;
+use PHPUnit\Framework\TestCase;
+
+class RandomCharGeneratorTest extends TestCase
+{
+    /**
+     * @expectedException \GpsLab\Component\Base64UID\Exception\InvalidArgumentException
+     */
+    public function testNegativeUidLength()
+    {
+        $generator = new RandomCharGenerator(-1);
+    }
+
+    /**
+     * @expectedException \GpsLab\Component\Base64UID\Exception\InvalidArgumentException
+     */
+    public function testZeroUidLength()
+    {
+        $generator = new RandomCharGenerator(0);
+    }
+
+    /**
+     * @expectedException \GpsLab\Component\Base64UID\Exception\InvalidArgumentException
+     */
+    public function testInvalidTypeOfCharset()
+    {
+        $generator = new RandomCharGenerator(1, 1);
+    }
+
+    public function testGenerateDefault()
+    {
+        $generator = new RandomCharGenerator();
+        $id = $generator->generate();
+        $this->assertSame(10, strlen($id));
+        $this->assertRegExp('/^[-_a-zA-Z0-9]{10}$/', $id);
+    }
+
+    public function testGenerateCustomLength()
+    {
+        $generator = new RandomCharGenerator(6);
+        $id = $generator->generate();
+        $this->assertSame(6, strlen($id));
+        $this->assertRegExp('/^[-_a-zA-Z0-9]{6}$/', $id);
+    }
+
+    public function testGenerateCustomCharset()
+    {
+        $charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/';
+        $generator = new RandomCharGenerator(11, $charset);
+        $id = $generator->generate();
+        $this->assertSame(11, strlen($id));
+        $this->assertRegExp('/^[\/+a-zA-Z0-9]{11}$/', $id);
+    }
+
+    public function testGenerateCustomCharset2()
+    {
+        $charset = '0123456789abcdef';
+        $generator = new RandomCharGenerator(11, $charset);
+        $id = $generator->generate();
+        $this->assertSame(11, strlen($id));
+        $this->assertRegExp('/^[a-f0-9]{11}$/', $id);
+    }
+}
