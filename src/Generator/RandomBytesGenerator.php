522da7d57cdab0bc8531c6383ddb859f3fcff6b3
Peter Gribanov <wcode404@gmail.com>
2019-12-26 18:39:04 +0300

    create RandomBytesGenerator

---
+++ src/Generator/RandomBytesGenerator.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Base64UID\Generator;
+
+use GpsLab\Component\Base64UID\Exception\InvalidArgumentException;
+
+class RandomBytesGenerator implements Generator
+{
+    /**
+     * @var int
+     */
+    private $bytes_length;
+
+    /**
+     * @param int $bytes_length
+     */
+    public function __construct($bytes_length = 8)
+    {
+        if (!is_int($bytes_length)) {
+            throw new InvalidArgumentException(sprintf('Length of bytes should be integer, got "%s" instead.', gettype($bytes_length)));
+        }
+        if ($bytes_length <= 0) {
+            throw new InvalidArgumentException(sprintf('Length of bytes should be grate then 0, got "%d" instead.', $bytes_length));
+        }
+
+        $this->bytes_length = $bytes_length;
+    }
+
+    /**
+     * @return string
+     */
+    public function generate()
+    {
+        $uid = random_bytes($this->bytes_length);
+        $uid = base64_encode($uid);
+        $uid = str_replace('=', '', $uid);
+
+        return $uid;
+    }
+}
---
+++ tests/Generator/RandomBytesGeneratorTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Base64UID\Tests\Generator;
+
+use GpsLab\Component\Base64UID\Generator\RandomBytesGenerator;
+use PHPUnit\Framework\TestCase;
+
+class RandomBytesGeneratorTest extends TestCase
+{
+    /**
+     * @expectedException \GpsLab\Component\Base64UID\Exception\InvalidArgumentException
+     */
+    public function testBytesLengthNoInteger()
+    {
+        $generator = new RandomBytesGenerator('8');
+    }
+
+    /**
+     * @expectedException \GpsLab\Component\Base64UID\Exception\InvalidArgumentException
+     */
+    public function testZeroBytesLength()
+    {
+        $generator = new RandomBytesGenerator(0);
+    }
+
+    public function testGenerateDefault()
+    {
+        $generator = new RandomBytesGenerator();
+        $id = $generator->generate();
+        $this->assertRegExp('/^[\/+a-zA-Z0-9]+$/', $id);
+    }
+}
